8077400faf60ed10280350cbea6e1d7cddf33df9
Praveena <praveenasenthilkumar340@gmail.com>
2025-04-16 05:50:35 +0000

    changes made

--- src/main/java/com/example/demo/DemoAppApplication.java
+++ src/main/java/com/example/demo/DemoAppApplication.java
@@ -1,20 +1,14 @@
-package com.example.demoapp;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
 public class DemoAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoAppApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "🚀 Hello from Demo App deployed using Jenkins & Kubernetes!";
-    }
 }
 
